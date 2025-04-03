6baa2ec2123d092fb4a47b315c293f28ba5574af
Mateus Espíndola <101195071+espindoolaa@users.noreply.github.com>
2025-04-03 02:43:55 -0300

    feat/final_version_alu

--- ula.v
+++ ula.v
@@ -1,19 +1,41 @@
-module ula ( // recieve the values and executes
+module ula (
     input [2:0] opcode,
     input signed [15:0] valor1,
     input signed [15:0] valor2,
-    output reg signed [15:0] resultado
-    );
-
-    always @(*) begin 
-        case(opcode)
-            3'b000: resultado = valor2; //caso em que ela só recebe
-            3'b001: resultado = valor1 + valor2; //soma
-            3'b010: resultado = valor1 + valor2; //soma com immediatos
-            3'b011: resultado = valor1 - valor2; //subtração
-            3'b100: resultado = valor1 - valor2; //subtração com immediatos
-            3'b101: resultado = valor1 * valor2; //multiplicação;
+    output reg signed [15:0] resultado,
+    output reg executou
+);
+    always @(*) begin
+        executou = 0;
+        case (opcode)
+            3'b000: begin 
+                resultado = valor2; 
+                executou = 1; 
+            end
+            3'b001: begin 
+                resultado = valor1 + valor2; 
+                executou = 1; 
+            end
+            3'b010: begin 
+                resultado = valor1 + valor2; 
+                executou = 1; 
+            end
+            3'b011: begin 
+                resultado = valor1 - valor2; 
+                executou = 1; 
+            end
+            3'b100: begin
+                resultado = valor1 - valor2; 
+                executou = 1; 
+            end
+            3'b101: begin
+                resultado = valor1 * valor2; 
+                executou = 1; 
+            end    
+            default: begin
+                resultado = 16'b0;
+                executou = 0;
+            end
         endcase
     end
-    
 endmodule
